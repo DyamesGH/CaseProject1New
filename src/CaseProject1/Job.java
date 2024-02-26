b8db38fed34a9a3c9dc18b7db01f4f80d08dd550
John Jomar Dimaunahan <johndjomar@gmail.com>
2024-02-27 07:01:10 +0800

    Added default constructors and settger getters:

--- src/CaseProject1/Job.java
+++ src/CaseProject1/Job.java
@@ -14,11 +14,12 @@ public class Job {
 	private static int counter = 1024;
 	private ArrayList<Material> materialsList = new ArrayList<Material>();
 	
-	
-	
+	//Default/Empty constructor
+	public Job() {
+		
+	}
 	
 	public Job(String jobDescription, float jobTimeEstimate, float jobTotalTime, Date jobStartDate, String customerName){
-		
 		this.jobNumber = counter++;
 		this.jobDescription = jobDescription;
 		this.jobTimeEstimate = jobTimeEstimate;
@@ -35,5 +36,69 @@ public class Job {
 		for (Material i : materialsList) {
 			System.out.println(i);
 		}
+	}
+
+	public int getJobNumber() {
+		return jobNumber;
+	}
+
+	public void setJobNumber(int jobNumber) {
+		this.jobNumber = jobNumber;
+	}
+
+	public String getJobDescription() {
+		return jobDescription;
+	}
+
+	public void setJobDescription(String jobDescription) {
+		this.jobDescription = jobDescription;
+	}
+
+	public float getJobTimeEstimate() {
+		return jobTimeEstimate;
+	}
+
+	public void setJobTimeEstimate(float jobTimeEstimate) {
+		this.jobTimeEstimate = jobTimeEstimate;
+	}
+
+	public float getJobTotalTime() {
+		return jobTotalTime;
+	}
+
+	public void setJobTotalTime(float jobTotalTime) {
+		this.jobTotalTime = jobTotalTime;
+	}
+
+	public Date getJobStartDate() {
+		return jobStartDate;
+	}
+
+	public void setJobStartDate(Date jobStartDate) {
+		this.jobStartDate = jobStartDate;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public static int getCounter() {
+		return counter;
+	}
+
+	public static void setCounter(int counter) {
+		Job.counter = counter;
+	}
+
+	public ArrayList<Material> getMaterialsList() {
+		return materialsList;
+	}
+
+	public void setMaterialsList(ArrayList<Material> materialsList) {
+		this.materialsList = materialsList;
 	}
 }
